719a0f021d0f473f04143b08132830685c9765ef
modality <modality@gmail.com>
2019-11-10 08:00:01 +0100

    pass LLD option to strip wasm from unnecessary sections

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 echo $1
 
-cargo build --manifest-path=contracts/$1/Cargo.toml --release --target wasm32-unknown-unknown &&
+WASM_BUILD_RUSTFLAGS='-C link-arg=-s' cargo build --manifest-path=contracts/$1/Cargo.toml --release --target wasm32-unknown-unknown &&
 
 cargo test $1 -- --nocapture
 
-			
+
