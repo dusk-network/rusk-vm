1d118e3dcc892cd11460342c2ca70ff08b2b2d7b
Jules de Smit <julesdesmit@gmail.com>
2020-03-25 15:47:25 +0100

    Update fee contract
    - Change all u128 to u64, as Phoenix works only with u64
    - Change the note creation and transfer call, to just be a "credit" function,
    which both creates the note, and stores it for the given public key
    - Add extra comments

--- tests/contracts/fee/wasm/src/lib.rs
+++ tests/contracts/fee/wasm/src/lib.rs
@@ -1,6 +1,5 @@
 #![no_std]
-use dusk_abi::{self, encoding, ContractCall, Signature, CALL_DATA_SIZE, H256};
-use phoenix_abi::{Note, Nullifier};
+use dusk_abi::{self, encoding, Provisioners, Signature, CALL_DATA_SIZE, H256};
 use serde::{Deserialize, Serialize};
 
 const TRANSFER_CONTRACT: [u8; 1] = [0u8];
@@ -10,12 +9,12 @@ pub enum FeeCall {
     Withdraw {
         sig: Signature,
         address: [u8; 32],
-        value: u128,
-        r: [u8; 32],
+        value: u64,
         pk: [u8; 32],
     },
     Distribute {
-        r: [u8; 32],
+        total_reward: u64,
+        addresses: Provisioners,
         pk: [u8; 32],
     },
     GetBalanceAndNonce {
@@ -23,6 +22,7 @@ pub enum FeeCall {
     },
 }
 
+// TODO: phoenix works with u64, but it would be more advisable to work with u128.
 #[no_mangle]
 pub fn call() {
     let mut buffer = [0u8; CALL_DATA_SIZE];
@@ -32,15 +32,16 @@ pub fn call() {
             sig,
             address,
             value,
-            r,
             pk,
         } => {
-            let (nonce, current_reward) = dusk_abi::get_storage(&address)
-                .unwrap_or((0 as u32, 0 as u128));
+            // Ensure provisioner is allowed to withdraw this amount
+            let (nonce, current_reward) =
+                dusk_abi::get_storage(&address).unwrap_or((0 as u32, 0 as u64));
             if current_reward < value {
                 panic!("insufficient funds");
             }
 
+            // Verify provisioner signature
             let mut verify_buf = [0u8; 4 + 16];
             let encoded =
                 encoding::encode(&(nonce, value), &mut verify_buf).unwrap();
@@ -48,70 +49,62 @@ pub fn call() {
                 panic!("invalid signature!");
             }
 
+            // Remove withdrawn amount from provisioner allotment
             dusk_abi::set_storage(
                 &address,
                 (nonce + 1, current_reward - value),
             );
 
-            let note = phoenix_abi::create_note(value, r, address);
-            let call = ContractCall::<(
-                [Nullifier; Nullifier::MAX],
-                [Note; Note::MAX],
-            )>::new((
-                [Nullifier; Nullifier::MAX],
-                notes,
-            ))
-            .unwrap();
-
-            let transfer_address =
-                dusk_abi::get_storage(&TRANSFER_CONTRACT).unwrap();
-            dusk_abi::call_contract(&transfer_address, 0, &call);
+            // Credit provisioner with withdrawn value
+            if !phoenix_abi::credit(value, &pk) {
+                panic!("could not credit provisioner")
+            }
         }
-        FeeCall::Distribute { r, pk } => {
+        FeeCall::Distribute {
+            total_reward,
+            addresses,
+            pk,
+        } => {
             // TODO: Check that calling tx is a coinbase
 
-            allocate_provisioner_rewards();
-
-            // TODO: implement mint function on transfer contract
-            let call = ContractCall::<()>::new_raw(mint);
-            let transfer_address =
-                dusk_abi::get_storage(&TRANSFER_CONTRACT).unwrap();
-            dusk_abi::call_contract(&transfer_address, 0, &call);
-
             // TODO: implement formula for bg reward derivation
-            let block_gen_reward = total_reward / 100;
+            let block_gen_reward = total_reward / 10;
 
-            let note = phoenix_abi::create_note(block_gen_reward, r, pk);
-            let notes = [Note::default(); Note::MAX];
-            notes[0] = note;
-            let call = ContractCall::<(
-                [Nullifier; Nullifier::MAX],
-                [Note; Note::MAX],
-            )>::new((
-                [Nullifier; Nullifier::MAX],
-                notes,
-            ))
-            .unwrap();
+            if !phoenix_abi::credit(block_gen_reward, &pk) {
+                panic!("could not credit block generator")
+            }
 
-            dusk_abi::call_contract(&transfer_address, 0, &call);
+            // Get reward by dividing the total reward minus the block generator
+            // reward by the amount of provisioners
+            let provisioners_count = {
+                let mut count = 0;
+                addresses.0.chunks(32).for_each(|a| {
+                    if a != [0u8; 32] {
+                        count += 1;
+                    }
+                });
+
+                count
+            };
+
+            // Allocate leftover rewards equally to all provisioners
+            let reward = (total_reward - block_gen_reward) / provisioners_count;
+            allocate_provisioner_rewards(reward, addresses);
         }
         FeeCall::GetBalanceAndNonce { address } => {
             dusk_abi::ret(
-                dusk_abi::get_storage(&address)
-                    .unwrap_or((0 as u32, 0 as u128)),
+                dusk_abi::get_storage(&address).unwrap_or((0 as u32, 0 as u64)),
             );
         }
     }
 }
 
-fn allocate_provisioner_rewards(total_reward: u128, block_height: u64) {
-    let h = block_height - 1;
-    let cert = dusk_abi::get_cert(h);
-
-    let reward = total_reward / cert.addresses().len();
-    cert.addresses().iter().for_each(|a| {
+// This function increases the reward amount for each provisioner address
+// in `addresses` by `reward`.
+fn allocate_provisioner_rewards(reward: u64, addresses: Provisioners) {
+    addresses.0.chunks(32).for_each(|a| {
         let (nonce, current_reward) =
-            dusk_abi::get_storage(a).unwrap_or((0 as u32, 0 as u128));
+            dusk_abi::get_storage(a).unwrap_or((0 as u32, 0 as u64));
         let new_reward = current_reward + reward;
         dusk_abi::set_storage(a, (nonce, new_reward));
     });
