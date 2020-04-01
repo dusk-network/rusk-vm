d78419b69d1bcb1448eb584c2d63bd3a2e29a39f
Jules de Smit <julesdesmit@gmail.com>
2020-04-01 15:53:31 +0200

    Add Approve and TransferFrom functionality to Transfer contract
    This is currently only available for transparent notes

--- tests/contracts/transfer/wasm/src/lib.rs
+++ tests/contracts/transfer/wasm/src/lib.rs
@@ -1,6 +1,28 @@
 #![no_std]
 use dusk_abi::{self, ContractCall};
-use phoenix_abi::{Note, Nullifier};
+use phoenix_abi::{Note, Nullifier, PublicKey};
+
+// TODO: obfuscated approve and transferfrom
+// TODO: proof verification
+pub enum TransferCall {
+    Transfer {
+        nullifiers: [Nullifier; Nullifier::MAX],
+        notes: [Note; Note::MAX],
+        // proof
+    },
+    Approve {
+        nullifiers: [Nullifier; Nullifier::MAX],
+        notes: [Note; Note::MAX],
+        pk: PublicKey,
+        value: u64,
+        // proof
+    },
+    TransferFrom {
+        sender: PublicKey,
+        recipient: PublicKey,
+        value: u64,
+    },
+}
 
 // Interface
 pub fn transfer(
@@ -12,11 +34,41 @@ pub fn transfer(
 
 #[no_mangle]
 pub fn call() {
-    let (nullifiers, notes): ([Nullifier; Nullifier::MAX], [Note; Note::MAX]) =
-        dusk_abi::args();
+    let data: TransferCall = dusk_abi::args();
+    match data {
+        TransferCall::Transfer { nullifiers, notes } => {
+            if !phoenix_abi::verify(&nullifiers, &notes) {
+                dusk_abi::ret(0);
+            }
+            dusk_abi::ret(phoenix_abi::store(&nullifiers, &notes));
+        }
+        TransferCall::Approve {
+            nullifiers,
+            notes,
+            pk,
+            value,
+        } => {
+            if !phoenix_abi::verify(&nullifiers, &notes) {
+                dusk_abi::ret(0);
+            }
 
-    if !phoenix_abi::verify(&nullifiers, &notes) {
-        dusk_abi::ret(0);
+            phoenix_abi::store(&nullifiers, &notes);
+            dusk_abi::set_storage(pk, value);
+            dusk_abi::ret(1);
+        }
+        TransferCall::TransferFrom {
+            sender,
+            recipient,
+            value,
+        } => {
+            let approved_value = dusk_abi::get_storage(pk);
+            if value > approved_value {
+                dusk_abi::ret(0);
+            }
+
+            dusk_abi::set_storage(pk, approved_value - value);
+            phoenix_abi::credit(value, recipient);
+            dusk_abi::ret(1);
+        }
     }
-    dusk_abi::ret(phoenix_abi::store(&nullifiers, &notes));
 }
