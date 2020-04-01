24bd46855811157846ba3f2aefe3447f4b8a43d0
Jules de Smit <julesdesmit@gmail.com>
2020-04-01 23:17:26 +0200

    Add missing methods, and fix some TODOs

--- tests/contracts/staking/wasm/src/lib.rs
+++ tests/contracts/staking/wasm/src/lib.rs
@@ -1,14 +1,21 @@
 #![no_std]
-use dusk_abi::{self, ContractCall, Signature, StakingCall, TransferCall};
+use dusk_abi::{
+    self, encoding, ContractCall, Signature, StakingCall, TransferCall, H256,
+};
 use phoenix_abi::{Note, Nullifier, PublicKey};
 
 const TRANSFER_CONTRACT: [u8; 1] = [0u8];
+const PUBLIC_KEY: [u8; 1] = [1u8];
 
 #[no_mangle]
 pub fn call() {
     let data: StakingCall = dusk_abi::args();
 
     match data {
+        StakingCall::Init { address, pk } => {
+            dusk_abi::set_storage(&TRANSFER_CONTRACT, address);
+            dusk_abi::set_storage(&PUBLIC_KEY, pk);
+        }
         StakingCall::Stake {
             nullifiers,
             notes,
@@ -17,9 +24,14 @@ pub fn call() {
             pk_bls,
             expiration,
             value,
+            current_height,
         } => {
-            (_value, _pk_bls, _deposit_height, expiry_height) =
-                dusk_abi::get_storage(identity);
+            let (_value, _pk_bls, _deposit_height, expiry_height): (
+                u64,
+                [u8; 32],
+                u64,
+                u64,
+            ) = dusk_abi::get_storage(&pk).unwrap();
             if expiry_height > current_height {
                 panic!("already an active stake for this identity");
             }
@@ -30,9 +42,17 @@ pub fn call() {
             }
 
             // Transfer the given notes to the staking contract
-            // TODO: devise contract pk
-            let call = TransferCall::Approve(nullifiers, notes, pk, value);
-            let address: H256 = dusk_abi::get_storage(&TRANSFER_CONTRACT);
+            let contract_pk = dusk_abi::get_storage(&PUBLIC_KEY).unwrap();
+            let call: ContractCall<bool> =
+                ContractCall::new(TransferCall::Approve {
+                    nullifiers,
+                    notes,
+                    pk: contract_pk,
+                    value,
+                })
+                .unwrap();
+            let address: H256 =
+                dusk_abi::get_storage(&TRANSFER_CONTRACT).unwrap();
             if !dusk_abi::call_contract(&address, 0, &call) {
                 panic!("could not transfer notes");
             }
@@ -49,25 +69,38 @@ pub fn call() {
             // proof,
             pk,
             sig,
+            current_height,
         } => {
-            (value, _pk_bls, deposit_height, expiry_height) =
-                dusk_abi::get_storage(identity);
+            let (value, _pk_bls, deposit_height, expiry_height): (
+                u64,
+                [u8; 32],
+                u64,
+                u64,
+            ) = dusk_abi::get_storage(&pk).unwrap();
             if expiry_height > current_height {
                 panic!("stake is still active for this identity");
             }
 
-            let mut verify_buf = [0u8; 32 + 8];
-            let encoded =
-                encoding::encode(&(pk, deposit_height), &mut verify_buf)
-                    .unwrap();
-            if !dusk_abi::verify_ed25519_signature(&pk, &sig, encoded) {
-                panic!("invalid signature");
-            }
+            // TODO: actually implement this
+            // let mut verify_buf = [0u8; 32 + 8];
+            // let encoded =
+            //     encoding::encode(&(pk, deposit_height), &mut verify_buf)
+            //         .unwrap();
+            // if !dusk_abi::verify_ed25519_signature(&pk, &sig, encoded) {
+            //     panic!("invalid signature");
+            // }
 
             // call transferfrom
-            // TODO: devise contract pk
-            let call = TransferCall::TransferFrom(pk, value);
-            let address: H256 = dusk_abi::get_storage(&TRANSFER_CONTRACT);
+            let contract_pk = dusk_abi::get_storage(&PUBLIC_KEY).unwrap();
+            let call: ContractCall<bool> =
+                ContractCall::new(TransferCall::TransferFrom {
+                    sender: contract_pk,
+                    recipient: pk,
+                    value: value,
+                })
+                .unwrap();
+            let address: H256 =
+                dusk_abi::get_storage(&TRANSFER_CONTRACT).unwrap();
             if !dusk_abi::call_contract(&address, 0, &call) {
                 panic!("could not refund stake");
             }
@@ -83,8 +116,12 @@ pub fn call() {
             msg1,
             msg2,
         } => {
-            (value, pk_bls, _deposit_height, expiry_height) =
-                dusk_abi::get_storage(pk);
+            let (value, pk_bls, _deposit_height, expiry_height): (
+                u64,
+                [u8; 32],
+                u64,
+                u64,
+            ) = dusk_abi::get_storage(&pk).unwrap();
             if value == 0 {
                 panic!("trying to slash a non-existant staker");
             }
@@ -116,10 +153,16 @@ pub fn call() {
             dusk_abi::set_storage(pk, (0, [0u8; 32], 0, 0));
             dusk_abi::ret(true);
         }
+        StakingCall::GetStake { pk } => {
+            let values: (u64, [u8; 32], u64, u64) =
+                dusk_abi::get_storage(&pk).unwrap();
+            dusk_abi::ret(values);
+        }
     }
 }
 
-pub fn deploy(address: H256) {
+#[no_mangle]
+pub fn deploy(address: H256, pk: PublicKey) {
     // Set transfer contract address, for later reference.
     dusk_abi::set_storage(&TRANSFER_CONTRACT, address);
 }
