b5bc4dc5e3d29e03b48682aa757358f4f190ac1d
Kristoffer Ström <kristoffer@rymdkoloni.se>
2020-04-20 16:49:54 +0200

    Rebase on master

--- dusk-abi/src/types.rs
+++ dusk-abi/src/types.rs
@@ -38,73 +38,46 @@ impl core::fmt::Debug for H256 {
     }
 }
 
-/// The `TransferCall` type.
-/// All the possible methods available for the `Transfer` Genesis Contract.
-#[derive(Serialize, Deserialize, Debug)]
-pub enum TransferCall {
-    /// The `Transfer` call arguments
-    Transfer {
-        /// The inputs for the Transfer call
-        inputs: [Input; Input::MAX],
-        /// The notes for the Transfer call
-        notes: [Note; Note::MAX],
-        /// The proof for the Transfer call
-        proof: Proof,
-    },
-    /// The `Approve` call arguments
-    Approve {
-        /// The inputs for the Approve call
-        inputs: [Input; Input::MAX],
-        /// The notes for the Approve call
-        notes: [Note; Note::MAX],
-        /// The Public Key for the Approve call
-        pk: PublicKey,
-        /// The transparent value for the Approve call
-        value: u64,
-        /// The proof for the Approve call
-        proof: Proof,
-    },
-    /// The `TransferFrom` call arguments
-    TransferFrom {
-        /// The sender's public key used for TransferFrom call
-        sender: PublicKey,
-        /// The recipient's public key used for TransferFrom call
-        recipient: PublicKey,
-        /// The transparent value for the TransferFrom call
-        value: u64,
-    },
+const SIGNATURE_BYTES: usize = 64;
+
+/// Standard 64 byte signature type
+#[repr(C)]
+pub struct Signature([u8; SIGNATURE_BYTES]);
+
+impl Signature {
+    /// Create a new signature from a byte slice
+    pub fn from_slice(slice: &[u8]) -> Self {
+        let mut buf = [0u8; 64];
+        buf.copy_from_slice(slice);
+        Signature(buf)
+    }
+
+    /// Returns a reference to the internal byte array
+    pub fn as_array_ref(&self) -> &[u8; 64] {
+        &self.0
+    }
 }
 
-/// The `FeeCall` type.
-/// All the possible methods available for the `Fee` Genesis Contract.
-#[derive(Serialize, Deserialize, Debug)]
-pub enum FeeCall {
-    /// The Withdraw call arguments
-    Withdraw {
-        /// The signature used for Withdraw call
-        sig: Signature,
-        /// The address used for Withdraw call
-        address: [u8; 32],
-        /// The value used for Withdraw call
-        value: u64,
-        /// The public key used for Withdraw call
-        pk: PublicKey,
-    },
-    /// The Distribute call arguments
-    Distribute {
-        /// The total reward to distribute
-        total_reward: u64,
-        /// The list of provisioners
-        addresses: Provisioners,
-        /// The Public Key of the block generator, to distribute a portion of
-        /// the reward to
-        pk: PublicKey,
-    },
-    /// The GetBalanceAndNonce call argument
-    GetBalanceAndNonce {
-        /// The address to get the balance and nonce from
-        address: [u8; 32],
-    },
+impl AsRef<[u8]> for Signature {
+    fn as_ref(&self) -> &[u8] {
+        &self.0
+    }
+}
+
+impl AsMut<[u8]> for Signature {
+    fn as_mut(&mut self) -> &mut [u8] {
+        &mut self.0
+    }
+}
+
+impl core::fmt::Debug for Signature {
+    fn fmt(&self, f: &mut core::fmt::Formatter) -> core::fmt::Result {
+        write!(f, "Signature(")?;
+        for i in 0..64 {
+            write!(f, "{:02x}", self.0[i])?;
+        }
+        Ok(())
+    }
 }
 
 #[cfg(feature = "std")]
--- src/ops/phoenix_ops.rs
+++ src/ops/phoenix_ops.rs
@@ -5,7 +5,6 @@ use std::convert::{TryFrom, TryInto};
 use std::env;
 use std::path::Path;
 
-use dusk_abi::encoding;
 use kelvin::ByteHash;
 use phoenix::{
     db, utils, zk, BlsScalar, NoteGenerator, NoteVariant, PublicKey,
@@ -36,75 +35,7 @@ impl<S: Resolver<H>, H: ByteHash> AbiCall<S, H> for PhoenixStore {
         context: &mut CallContext<S, H>,
         args: RuntimeArgs,
     ) -> Result<Option<RuntimeValue>, VMError> {
-        let inputs_ptr = args.get(0)?;
-        let notes_ptr = args.get(1)?;
-        let proof_ptr = args.get(2)?;
-
-        context
-            .top()
-            .memory
-            .with_direct_access_mut::<Result<Option<RuntimeValue>, VMError>, _>(
-                |a| {
-                    let inputs_buf =
-                        &a[inputs_ptr..inputs_ptr + (Input::MAX * Input::SIZE)];
-                    let inputs: Result<Vec<TransactionInput>, fermion::Error> =
-                        inputs_buf
-                            .chunks(Input::SIZE)
-                            .take_while(|bytes| has_value(bytes))
-                            .map(|bytes| {
-                                let input: Input = encoding::decode(bytes)?;
-                                let item: TransactionInput =
-                                    input.try_into().map_err(|_| {
-                                        fermion::Error::InvalidRepresentation
-                                    })?;
-                                Ok(item)
-                            })
-                            .collect();
-
-                    let notes_buf =
-                        &a[notes_ptr..notes_ptr + (Note::MAX * Note::SIZE)];
-
-                    let notes: Result<Vec<TransactionOutput>, fermion::Error> =
-                        notes_buf
-                            .chunks(Note::SIZE)
-                            .take_while(|bytes| has_value(bytes))
-                            .map(|bytes| {
-                                let note: Note = encoding::decode(bytes)?;
-                                Ok(TransactionOutput::try_from(note).map_err(
-                                    |_| fermion::Error::InvalidRepresentation,
-                                )?)
-                            })
-                            .collect();
-
-                    let proof_buf = &a[proof_ptr..proof_ptr + Proof::SIZE];
-                    let proof = zk::bytes_to_proof(&proof_buf).unwrap();
-
-                    let mut tx = Transaction::default();
-
-                    inputs?
-                        .into_iter()
-                        .for_each(|input| tx.push_input(input).unwrap());
-
-                    let mut notes = notes.unwrap();
-
-                    let fee_note = notes.pop().unwrap();
-                    tx.set_fee(fee_note);
-
-                    notes
-                        .into_iter()
-                        .for_each(|note| tx.push_output(note).unwrap());
-
-                    tx.set_proof(proof);
-
-                    match db::store(
-                        Path::new(&env::var("PHOENIX_DB").unwrap()),
-                        &tx,
-                    ) {
-                        Ok(_) => SUCCESS,
-                        Err(_) => FAIL,
-                    }
-                },
-            )
+        todo!("port phoenixstore");
     }
 }
 
@@ -119,72 +50,7 @@ impl<S: Resolver<H>, H: ByteHash> AbiCall<S, H> for PhoenixVerify {
         context: &mut CallContext<S, H>,
         args: RuntimeArgs,
     ) -> Result<Option<RuntimeValue>, VMError> {
-        let inputs_ptr = args.get(0)?;
-        let notes_ptr = args.get(1)?;
-        let proof_ptr = args.get(2)?;
-
-        context
-            .top()
-            .memory
-            .with_direct_access_mut::<Result<Option<RuntimeValue>, VMError>, _>(
-                |a| {
-                    let inputs_buf =
-                        &a[inputs_ptr..inputs_ptr + (Input::MAX * Input::SIZE)];
-                    let inputs: Result<Vec<TransactionInput>, fermion::Error> =
-                        inputs_buf
-                            .chunks(Input::SIZE)
-                            .take_while(|bytes| has_value(bytes))
-                            .map(|bytes| {
-                                let input: Input = encoding::decode(bytes)?;
-                                let item: TransactionInput =
-                                    input.try_into().map_err(|_| {
-                                        fermion::Error::InvalidRepresentation
-                                    })?;
-                                Ok(item)
-                            })
-                            .collect();
-
-                    let notes_buf =
-                        &a[notes_ptr..notes_ptr + (Note::MAX * Note::SIZE)];
-
-                    let notes: Result<Vec<TransactionOutput>, fermion::Error> =
-                        notes_buf
-                            .chunks(Note::SIZE)
-                            .take_while(|bytes| has_value(bytes))
-                            .map(|bytes| {
-                                let note: Note = encoding::decode(bytes)?;
-                                Ok(TransactionOutput::try_from(note).map_err(
-                                    |_| fermion::Error::InvalidRepresentation,
-                                )?)
-                            })
-                            .collect();
-
-                    let proof_buf = &mut a[proof_ptr..proof_ptr + Proof::SIZE];
-                    let proof = zk::bytes_to_proof(&proof_buf).unwrap();
-
-                    let mut tx = Transaction::default();
-
-                    inputs?
-                        .into_iter()
-                        .for_each(|input| tx.push_input(input).unwrap());
-
-                    let mut notes = notes.unwrap();
-
-                    let fee_note = notes.pop().unwrap();
-                    tx.set_fee(fee_note);
-
-                    notes
-                        .into_iter()
-                        .for_each(|note| tx.push_output(note).unwrap());
-
-                    tx.set_proof(proof);
-
-                    match tx.verify() {
-                        Ok(_) => SUCCESS,
-                        Err(_) => FAIL,
-                    }
-                },
-            )
+        todo!("port phoenixverify");
     }
 }
 
@@ -201,42 +67,6 @@ impl<S: Resolver<H>, H: ByteHash> AbiCall<S, H> for PhoenixCredit {
         context: &mut CallContext<S, H>,
         args: RuntimeArgs,
     ) -> Result<Option<RuntimeValue>, VMError> {
-        let reward = args.get(0)?;
-        let pk_ptr = args.get(1)?;
-
-        context
-            .top()
-            .memory
-            .with_direct_access_mut::<Result<Option<RuntimeValue>, VMError>, _>(
-                |a| {
-                    let pk_bytes = &a[pk_ptr..pk_ptr + 64];
-                    let pk = PublicKey::new(
-                        utils::deserialize_compressed_jubjub(&pk_bytes[0..32])
-                            .unwrap(),
-                        utils::deserialize_compressed_jubjub(&pk_bytes[32..64])
-                            .unwrap(),
-                    );
-
-                    let (output, _) =
-                        TransparentNote::output(&pk, reward as u64);
-
-                    let mut tx = Transaction::default();
-                    let item = TransactionOutput::new(
-                        NoteVariant::Transparent(output),
-                        reward as u64,
-                        BlsScalar::default(),
-                        PublicKey::default(),
-                    );
-                    tx.push_output(item).unwrap();
-
-                    match db::store(
-                        Path::new(&env::var("PHOENIX_DB").unwrap()),
-                        &tx,
-                    ) {
-                        Ok(_) => SUCCESS,
-                        Err(_) => FAIL,
-                    }
-                },
-            )
+        todo!("port phoenixcredit")
     }
 }
