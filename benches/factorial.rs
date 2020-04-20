f6537ef01a3dd696e1289f9ba2ad00d8ab748a6d
Kristoffer Ström <kristoffer@rymdkoloni.se>
2020-04-20 17:14:07 +0200

    Comment out to-port ops

--- benches/factorial.rs
+++ benches/factorial.rs
@@ -24,7 +24,7 @@ fn factorial_bench(c: &mut Criterion) {
     let code = include_bytes!(concat!(
         "../tests/contracts/",
         "factorial",
-        "/wasm/target/wasm32-unknown-unknown/release/",
+        "/target/wasm32-unknown-unknown/release/",
         "factorial",
         ".wasm"
     ));
