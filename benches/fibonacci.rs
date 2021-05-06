43c3214613421e83dd22c676de9640fadbc23655
moana <moana@dusk.network>
2021-05-06 15:39:13 +0200

    Update deps local path to crates.io versions
    - Update benchmarks

--- benches/fibonacci.rs
+++ benches/fibonacci.rs
@@ -4,45 +4,42 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use canonical::Id32;
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use rusk_vm::{Contract, GasMeter, NetworkState, Schedule, StandardABI};
+use rusk_vm::{Contract, ContractId, GasMeter, NetworkState};
+use fibonacci::Fibonacci;
 
 fn get_config() -> Criterion {
     Criterion::default().sample_size(10)
 }
 
-fn factorial_3(
-    network: &mut NetworkState<StandardABI<Blake2b>, Blake2b>,
-    contract_id: Id32,
+fn fibonacci_3(
+    network: &mut NetworkState,
+    contract_id: ContractId,
     gas: &mut GasMeter,
 ) {
     let n = 3;
 
     network
-        .call_contract::<u64, u64>(contract_id, n, gas)
+        .query::<_, u64>(contract_id, (fibonacci::COMPUTE, n), gas)
         .unwrap();
 }
 
-fn factorial_bench(c: &mut Criterion) {
+fn fibonacci_bench(c: &mut Criterion) {
     let code = include_bytes!(concat!(
-        "../tests/contracts/",
-        "factorial",
-        "/target/wasm32-unknown-unknown/release/",
-        "factorial",
+        "../target/wasm32-unknown-unknown/release/",
+        "fibonacci",
         ".wasm"
     ));
 
-    let schedule = Schedule::default();
-    let contract = Contract::new(code, &schedule).unwrap();
+    let contract = Contract::new(Fibonacci, code.to_vec());
 
-    let mut network = NetworkState::<StandardABI<_>, Blake2b>::default();
+    let mut network = NetworkState::default();
 
     let contract_id = network.deploy(contract).unwrap();
     let mut gas = GasMeter::with_limit(1_000_000_000_000);
-    c.bench_function("factorial 3", |b| {
+    c.bench_function("fibonacci 3", |b| {
         b.iter(|| {
-            factorial_3(
+            fibonacci_3(
                 black_box(&mut network),
                 black_box(contract_id),
                 black_box(&mut gas),
@@ -51,5 +48,5 @@ fn factorial_bench(c: &mut Criterion) {
     });
 }
 
-criterion_main!(factorial_main);
-criterion_group!(name = factorial_main; config = get_config(); targets = factorial_bench);
+criterion_main!(fibonacci_main);
+criterion_group!(name = fibonacci_main; config = get_config(); targets = fibonacci_bench);
