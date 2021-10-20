1680bbcee1f6a27197a66d9adbae7f6f4e35eeec
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-10-20 15:52:15 +0200

    Eliminate wasmi code phase 2

--- src/ops/gas.rs
+++ src/ops/gas.rs
@@ -4,37 +4,17 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use super::AbiCall;
 use crate::call_context::CallContext;
 use crate::VMError;
 
-use wasmi::{RuntimeArgs, RuntimeValue, ValueType};
 use crate::resolver::Env;
-use crate::NetworkState;
 
 pub struct Gas;
-
-impl AbiCall for Gas {
-    const ARGUMENTS: &'static [ValueType] = &[ValueType::I32];
-    const RETURN: Option<ValueType> = None;
-
-    fn call(
-        context: &mut CallContext,
-        args: RuntimeArgs,
-    ) -> Result<Option<RuntimeValue>, VMError> {
-        let meter = context.gas_meter_mut();
-        let gas: u32 = args.nth_checked(0)?;
-        if meter.charge(gas as u64).is_out_of_gas() {
-            return Err(VMError::OutOfGas);
-        }
-        Ok(None)
-    }
-}
 
 impl Gas {
     pub fn gas(env: &Env, gas_charged: u64) -> Result<(), VMError> {
         let context: &mut CallContext = unsafe { &mut *(env.context.0 as *mut CallContext)};
-        let meter = context.gas_meter_mut();
+        let mut meter = context.gas_meter_mut();
         if meter.charge(gas_charged).is_out_of_gas() {
             return Err(VMError::OutOfGas);
         }
@@ -48,23 +28,6 @@ impl GasConsumed {
     pub const GAS_CONSUMED_CALL_COST: u64 = 1;
 }
 
-impl AbiCall for GasConsumed {
-    const ARGUMENTS: &'static [ValueType] = &[];
-    const RETURN: Option<ValueType> = Some(ValueType::I64);
-
-    fn call(
-        context: &mut CallContext,
-        _args: RuntimeArgs,
-    ) -> Result<Option<RuntimeValue>, VMError> {
-        // FIXME: This will not always be correct since if the `gas_consumed =
-        // ALL` the gas, this will add the extra cost of the call
-        // which can't be consumed since it's not even there.
-        Ok(Some(RuntimeValue::from(
-            context.gas_meter().spent() + GasConsumed::GAS_CONSUMED_CALL_COST,
-        )))
-    }
-}
-
 impl GasConsumed {
     pub fn gas_consumed(env: &Env) -> Result<u64, VMError> {
         let context: &mut CallContext = unsafe { &mut *(env.context.0 as *mut CallContext)};
@@ -76,18 +39,6 @@ impl GasConsumed {
 }
 
 pub struct GasLeft;
-
-impl AbiCall for GasLeft {
-    const ARGUMENTS: &'static [ValueType] = &[];
-    const RETURN: Option<ValueType> = Some(ValueType::I64);
-
-    fn call(
-        context: &mut CallContext,
-        _args: RuntimeArgs,
-    ) -> Result<Option<RuntimeValue>, VMError> {
-        Ok(Some(RuntimeValue::from(context.gas_meter().left())))
-    }
-}
 
 impl GasLeft {
     pub fn gas_left(env: &Env) -> Result<u64, VMError> {
--- src/ops/query.rs
+++ src/ops/query.rs
@@ -5,62 +5,14 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use crate::call_context::CallContext;
-use crate::ops::AbiCall;
 use crate::VMError;
 
 use canonical::{Canon, Sink, Source};
 use dusk_abi::{ContractId, Query};
-use wasmi::{RuntimeArgs, RuntimeValue, ValueType};
 use crate::resolver::Env;
-use crate::NetworkState;
-use crate::gas::GasMeter;
 
 pub struct ExecuteQuery;
 
-impl AbiCall for ExecuteQuery {
-    const ARGUMENTS: &'static [ValueType] = &[ValueType::I32, ValueType::I32];
-    const RETURN: Option<ValueType> = None;
-
-    fn call(
-        context: &mut CallContext,
-        args: RuntimeArgs,
-    ) -> Result<Option<RuntimeValue>, VMError> {
-        if let [RuntimeValue::I32(contract_id_ofs), RuntimeValue::I32(query_ofs)] =
-            *args.as_ref()
-        {
-            let contract_id_ofs = contract_id_ofs as usize;
-            let query_ofs = query_ofs as usize;
-
-            let (contract_id, query) = context
-                .memory(|m| {
-                    let contract_id = ContractId::from(
-                        &m[contract_id_ofs..contract_id_ofs + 32],
-                    );
-
-                    let mut source = Source::new(&m[query_ofs..]);
-                    let query = Query::decode(&mut source)?;
-
-                    Ok((contract_id, query))
-                })
-                .map_err(VMError::from_store_error)?;
-
-            let result = context.query(contract_id, query)?;
-
-            context
-                .memory_mut(|m| {
-                    // write back the return value
-                    let mut sink = Sink::new(&mut m[query_ofs..]);
-                    result.encode(&mut sink);
-                    Ok(())
-                })
-                .map_err(VMError::from_store_error)?;
-
-            Ok(None)
-        } else {
-            Err(VMError::InvalidArguments)
-        }
-    }
-}
 
 impl ExecuteQuery {
     pub fn query(env: &Env, contract_id_ofs: u32, query_ofs: u32) -> Result<(), VMError> {
