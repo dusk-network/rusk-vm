2cc7f4f31ad2ad0e850a241fc1d04385fe3e4db7
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-10-25 16:14:14 +0200

    Clean up call context

--- src/call_context.rs
+++ src/call_context.rs
@@ -11,7 +11,6 @@ use crate::contract::ContractId;
 use crate::gas::GasMeter;
 use crate::state::NetworkState;
 use crate::VMError;
-
 use crate::resolver::{Env, ImportReference, HostImportsResolver};
 use crate::memory::WasmerMemory;
 
@@ -20,7 +19,6 @@ use wasmer_compiler_cranelift::Cranelift;
 use wasmer_engine_universal::Universal;
 
 use std::ffi::c_void;
-
 
 
 #[derive(Debug)]
@@ -65,14 +63,6 @@ impl StackFrame {
         }
     }
 
-    // fn memory<R, C: FnOnce(&[u8]) -> R>(&self, closure: C) -> R {
-    //     self.memory.with_direct_access(closure)
-    // }
-    //
-    // fn memory_mut<R, C: FnOnce(&mut [u8]) -> R>(&mut self, closure: C) -> R {
-    //     self.memory.with_direct_access_mut(closure)
-    // }
-
     fn write_memory(&mut self, source_slice: &[u8], offset: u64) -> Result<(), VMError>{
         unsafe { WasmerMemory::write_memory_bytes(self.memory.inner.get_unchecked(), offset, source_slice) }
     }
@@ -113,7 +103,6 @@ impl<'a> CallContext<'a> {
             context: ImportReference(self as *mut _ as *mut c_void)
         };
 
-        //let instance;
         let wasmer_instance: Instance;
 
         if let Some(module) = self.state.modules().borrow().get(&target) {
@@ -122,56 +111,21 @@ impl<'a> CallContext<'a> {
         } else {
             let contract = self.state.get_contract(&target)?;
 
-            //let module = wasmi::Module::from_buffer(contract.bytecode())?;
-            // WASMER
             let wasmer_store = Store::new(&Universal::new(Cranelift::default()).engine());
             let wasmer_module = Module::new(&wasmer_store, contract.bytecode()).expect("wasmer module created");
 
 
-            //instance = wasmi::ModuleInstance::new(&module, &imports)?.assert_no_start();
-
-
-            // WASMER
             let wasmer_import_names: Vec<String> = wasmer_module.imports().map(|i| i.name().to_string()).collect();
-            println!("query: import names for contract = {:?}", wasmer_import_names);
             let mut wasmer_import_object = ImportObject::new();
-            // WASMER env namespace
             let mut env_namespace = Exports::new();
             let mut canon_namespace = Exports::new();
 
             HostImportsResolver::insert_into_namespace(&mut env_namespace, &wasmer_store, env.clone(), &wasmer_import_names);
             HostImportsResolver::insert_into_namespace(&mut canon_namespace, &wasmer_store, env.clone(), &wasmer_import_names);
-            println!("query: register env and canon begin");
             wasmer_import_object.register("env", env_namespace);
             wasmer_import_object.register("canon", canon_namespace);
-            println!("query: register env and canon end");
 
-            // match instance.export_by_name("memory") {
-            //     Some(wasmi::ExternVal::Memory(memref)) => {
-            //         // write contract state and argument to memory
-            //         memref
-            //             .with_direct_access_mut(|m| {
-            //                 let mut sink = Sink::new(&mut *m);
-            //                 // copy the raw bytes only, since the
-            //                 // contract
-            //                 // can infer
-            //                 // it's own state and argument lengths
-            //                 sink.copy_bytes(contract.state().as_bytes());
-            //                 sink.copy_bytes(query.as_bytes());
-            //                 Ok(())
-            //             })
-            //             .map_err(VMError::from_store_error)?;
-            //
-            //         self.stack
-            //             .push(StackFrame::new_query(target, memref, query));
-            //     }
-            //     _ => return Err(VMError::MemoryNotFound),
-            // }
-
-            // WASMER
-            println!("query: instance creation begin");
             wasmer_instance = Instance::new(&wasmer_module, &wasmer_import_object)?;
-            println!("query: instance creation end");
 
             let mut wasmer_memory = WasmerMemory { inner: LazyInit::new() };
             wasmer_memory.init_env_memory(&wasmer_instance.exports)?;
@@ -182,31 +136,9 @@ impl<'a> CallContext<'a> {
                 .push(StackFrame::new_query(target, wasmer_memory, query));
         }
 
-        // Perform the query call
-        //instance.invoke_export("q", &[wasmi::RuntimeValue::I32(0)], self)?;
-
-        // WASMER
-        println!("query: before get native function q");
         let wasmer_run_func: NativeFunc<i32, ()> = wasmer_instance.exports.get_native_function("q").expect("wasmer invoked function q");
-        println!("query: after get native function q");
-        println!("query: before call q");
         wasmer_run_func.call(0)?;
-        println!("query: after call q");
 
-        // match instance.export_by_name("memory") {
-        //     Some(wasmi::ExternVal::Memory(memref)) => memref
-        //         .with_direct_access_mut(|m| {
-        //             let mut source = Source::new(&m[..]);
-        //             let result = ReturnValue::decode(&mut source)?;
-        //
-        //             self.stack.pop();
-        //             Ok(result)
-        //         })
-        //         .map_err(VMError::from_store_error),
-        //     _ => Err(VMError::MemoryNotFound),
-        // }
-
-        // WASMER
         let mut wasmer_memory = WasmerMemory { inner: LazyInit::new() };
         wasmer_memory.init_env_memory(&wasmer_instance.exports)?;
         let read_buffer = unsafe { WasmerMemory::read_memory_bytes(wasmer_memory.inner.get_unchecked(), 0, wasmer_memory.inner.get_unchecked().data_size() as usize)? };
@@ -225,64 +157,26 @@ impl<'a> CallContext<'a> {
             context: ImportReference(self as *mut _ as *mut c_void)
         };
 
-        //let instance;
         let wasmer_instance;
 
         {
             let contract = self.state.get_contract(&target_contract_id)?;
-            // let module = wasmi::Module::from_buffer(contract.bytecode())?;
 
-            // WASMER
             let wasmer_store = Store::new(&Universal::new(Cranelift::default()).engine());
             let wasmer_module = Module::new(&wasmer_store, contract.bytecode()).expect("wasmer module created");
 
-
-
-            // instance = wasmi::ModuleInstance::new(&module, &imports)?
-            //     .assert_no_start();
-            // WASMER
             let wasmer_import_names: Vec<String> = wasmer_module.imports().map(|i| i.name().to_string()).collect();
-            println!("transact: import names for contract = {:?}", wasmer_import_names);
             let mut wasmer_import_object = ImportObject::new();
-            // WASMER env namespace
             let mut env_namespace = Exports::new();
             let mut canon_namespace = Exports::new();
             HostImportsResolver::insert_into_namespace(&mut env_namespace, &wasmer_store, env.clone(), &wasmer_import_names);
             HostImportsResolver::insert_into_namespace(&mut canon_namespace, &wasmer_store, env.clone(), &wasmer_import_names);
 
-            println!("transact: register env and canon begin");
             wasmer_import_object.register("env", env_namespace);
             wasmer_import_object.register("canon", canon_namespace);
-            println!("transact: register env and canon end");
 
-            // WASMER
-            println!("transact: instance creation begin");
             wasmer_instance = Instance::new(&wasmer_module, &wasmer_import_object).expect("wasmer module created");
-            println!("transact: instance creation end");
 
-
-            // match instance.export_by_name("memory") {
-            //     Some(wasmi::ExternVal::Memory(memref)) => {
-            //         // write contract state and argument to memory
-            //
-            //         memref.with_direct_access_mut(|m| {
-            //             let mut sink = Sink::new(&mut *m);
-            //             // copy the raw bytes only, since the contract can
-            //             // infer it's own state and argument lengths.
-            //             sink.copy_bytes(contract.state().as_bytes());
-            //             sink.copy_bytes(transaction.as_bytes());
-            //         });
-            //
-            //         self.stack.push(StackFrame::new_transaction(
-            //             target,
-            //             memref,
-            //             transaction,
-            //         ));
-            //     }
-            //     _ => return Err(VMError::MemoryNotFound),
-            // }
-
-            // WASMER
             let mut wasmer_memory = WasmerMemory { inner: LazyInit::new() };
             wasmer_memory.init_env_memory(&wasmer_instance.exports)?;
             unsafe { WasmerMemory::write_memory_bytes(wasmer_memory.inner.get_unchecked(), 0, contract.state().as_bytes())? };
@@ -294,49 +188,15 @@ impl<'a> CallContext<'a> {
                 transaction,
             ));
         }
-        // Perform the transact call
-        // instance.invoke_export("t", &[wasmi::RuntimeValue::I32(0)], self)?;
 
-        // WASMER
-        println!("transact: before get native function t");
         let wasmer_run_func: NativeFunc<i32, ()> = wasmer_instance.exports.get_native_function("t").expect("wasmer invoked function t");
-        println!("transact: before get native function t");
-        println!("transact: before call t");
         wasmer_run_func.call(0)?;
-        println!("transact: after call t");
-
 
         let ret = {
-            println!("transact: getting contract from state");
             let mut contract = self.state.get_contract_mut(&target_contract_id)?;
-            println!("transact: gotten contract from state");
-
-            // match instance.export_by_name("memory") {
-            //     Some(wasmi::ExternVal::Memory(memref)) => {
-            //         memref
-            //             .with_direct_access_mut(|m| {
-            //                 let mut source = Source::new(&m[..]);
-            //
-            //                 // read new state
-            //                 let state = ContractState::decode(&mut source)?;
-            //
-            //                 // update new self state
-            //                 *(*contract).state_mut() = state;
-            //
-            //                 // read return value
-            //                 ReturnValue::decode(&mut source)
-            //             })
-            //             .map_err(VMError::from_store_error)
-            //     }
-            //     _ => return Err(VMError::MemoryNotFound),
-            // }
-
-            // WASMER
             let mut wasmer_memory = WasmerMemory { inner: LazyInit::new() };
             wasmer_memory.init_env_memory(&wasmer_instance.exports)?;
-            println!("transact: reading buffer from wasmer memory: {:?}", unsafe {wasmer_memory.inner.get_unchecked().data_size()} );
             let read_buffer = unsafe { WasmerMemory::read_memory_bytes(wasmer_memory.inner.get_unchecked(), 0, wasmer_memory.inner.get_unchecked().data_size() as usize)? };
-            println!("transact: read buffer from wasmer memory: {:?}", read_buffer.len() );
             let mut source = Source::new(&read_buffer);
             let state = ContractState::decode(&mut source).expect("query result decoded");
             *(*contract).state_mut() = state;
@@ -403,9 +263,4 @@ impl<'a> CallContext<'a> {
         &mut self.state
     }
 }
-
-// Convenience function to construct host traps
-// pub fn host_trap(host: VMError) -> Trap {
-//     Trap::new(TrapKind::Host(Box::new(host)))
-// }
 
