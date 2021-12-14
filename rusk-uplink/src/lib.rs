1d405e9e896d4bb0c3de742577568e29d3428aa8
Kristoffer Ström <kristoffer@rymdkoloni.se>
2021-12-14 11:57:37 +0100

    Integration start

---
+++ rusk-uplink/src/lib.rs
@@ -0,0 +1,25 @@
+mod ext {
+    #[link(wasm_import_module = "env")]
+    extern "C" {
+        pub fn debug(ofs: &u8, len: i32);
+    }
+}
+
+pub fn debug(string: &'static str) {
+    let bytes = string.as_bytes();
+    unsafe { ext::debug(&bytes[0], bytes.len() as i32) }
+}
+
+/// Store backend over FFI
+#[cfg(not(feature = "host"))]
+mod ffi_store;
+
+/// Store backend over FFI
+#[cfg(not(feature = "host"))]
+pub use ffi_store::*;
+
+pub mod helpers;
+pub use helpers::*;
+
+pub mod definitions;
+pub use definitions::*;
