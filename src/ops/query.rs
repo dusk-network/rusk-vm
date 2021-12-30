5f13fb855a8c6d79e4bcf415257e814d64b8ba6b
Milosz Muszynski <milosz.muszynski@gmail.com>
2021-12-30 09:48:36 +0100

    Correct cosmetics and deref in ops/query

--- src/ops/query.rs
+++ src/ops/query.rs
@@ -30,22 +30,28 @@ impl ExecuteQuery {
         let contract_id_ofs = contract_id_ofs as u64;
         let query_ofs = query_ofs as u64;
         let context = env.get_context();
+
         let contract_id_memory =
             context.read_memory(contract_id_ofs, size_of::<ContractId>())?;
         let contract_id = ContractId::from(&contract_id_memory);
+
         let query_memory =
             context.read_memory(query_ofs, query_len as usize)?;
-        let query_name =
-            context.read_memory(name_ofs as u64, name_len as usize)?;
         let mut query_data: AlignedVec = AlignedVec::new();
         query_data.extend_from_slice(query_memory);
+
+        let query_name =
+            context.read_memory(name_ofs as u64, name_len as usize)?;
         let name =
             str::from_utf8(query_name).map_err(|_| VMError::InvalidUtf8)?;
-        let raw_query = RawQuery::from(query_data, &name);
+
+        let raw_query = RawQuery::from(query_data, name);
         let mut gas_meter = context.gas_meter().limited(gas_limit);
         let context = env.get_context();
         let result = context.query(contract_id, raw_query, &mut gas_meter)?;
+
         context.write_memory(&result.0, query_ofs as u64);
+
         Ok(result.0.len() as u32)
     }
 }
