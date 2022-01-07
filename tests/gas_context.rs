fa7353cb9b018e83630415eb2df35c72eb6d944e
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-01-07 14:57:48 +0100

    Gas context test on and formatting

--- tests/gas_context.rs
+++ tests/gas_context.rs
@@ -5,9 +5,8 @@
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
 use gas_context::{GasContextData, SetGasLimits};
-use rusk_vm::{Contract, Gas, GasMeter, NetworkState};
 use microkelvin::HostStore;
-
+use rusk_vm::{Contract, Gas, GasMeter, NetworkState};
 
 #[test]
 fn gas_context() {
@@ -37,12 +36,7 @@ fn gas_context() {
     let call_gas_limits = vec![0; NUMBER_OF_NESTED_CALLS];
 
     network
-        .transact(
-            contract_id,
-            0,
-            SetGasLimits::new(call_gas_limits),
-            &mut gas,
-        )
+        .transact(contract_id, 0, SetGasLimits::new(call_gas_limits), &mut gas)
         .unwrap();
 
     network
@@ -55,12 +49,7 @@ fn gas_context() {
         .unwrap();
 
     let limits = network
-        .query(
-            contract_id,
-            0,
-            gas_context::ReadGasLimits,
-            &mut gas,
-        )
+        .query(contract_id, 0, gas_context::ReadGasLimits, &mut gas)
         .unwrap();
 
     let mut bounds: Vec<(u64, u64)> = limits
@@ -93,68 +82,63 @@ fn gas_context() {
     }
 }
 
-// #[ignore]
-// fn gas_context_with_call_limit() {
-//     let gas_context_data = GasContextData::new();
-//
-//     let code = include_bytes!(
-//         "../target/wasm32-unknown-unknown/release/gas_context.wasm"
-//     );
-//
-//     let store = HostStore::new();
-//     let contract = Contract::new(gas_context_data, code.to_vec());
-//
-//     let mut network = NetworkState::new();
-//
-//     let contract_id = network.deploy(contract).unwrap();
-//
-//     const INITIAL_GAS_LIMIT: Gas = 900_000_000;
-//
-//     let mut gas = GasMeter::with_limit(INITIAL_GAS_LIMIT);
-//
-//     let call_gas_limits: Vec<u64> =
-//         (100_000_000..800_000_000).step_by(100_000_000).collect();
-//     let mut upper_bounds = call_gas_limits.clone();
-//
-//     let number_of_nested_calls: usize = call_gas_limits.len();
-//
-//     network
-//         .transact::<_, Vec<u64>>(
-//             contract_id,
-//             0,
-//             (gas_context::SET_GAS_LIMITS, call_gas_limits),
-//             &mut gas,
-//         )
-//         .unwrap();
-//
-//     network
-//         .transact::<_, u64>(
-//             contract_id,
-//             0,
-//             (gas_context::COMPUTE, number_of_nested_calls as u64),
-//             &mut gas,
-//         )
-//         .unwrap();
-//
-//     let limits = network
-//         .query::<_, Vec<u64>>(
-//             contract_id,
-//             0,
-//             (gas_context::READ_GAS_LIMITS, ()),
-//             &mut gas,
-//         )
-//         .unwrap();
-//
-//     upper_bounds.remove(0);
-//     upper_bounds.reverse();
-//     upper_bounds.insert(0, INITIAL_GAS_LIMIT);
-//     for (index, limit) in limits.iter().enumerate() {
-//         assert!(
-//             limit < &upper_bounds[index],
-//             "Limit {} equal to {} should be below {}",
-//             index,
-//             limit,
-//             upper_bounds[index]
-//         );
-//     }
-// }
+#[test]
+fn gas_context_with_call_limit() {
+    let gas_context_data = GasContextData::new();
+
+    let code = include_bytes!(
+        "../target/wasm32-unknown-unknown/release/gas_context.wasm"
+    );
+
+    let store = HostStore::new();
+    let contract = Contract::new(&gas_context_data, code.to_vec(), &store);
+
+    let mut network = NetworkState::new(store);
+
+    let contract_id = network.deploy(contract).unwrap();
+
+    const INITIAL_GAS_LIMIT: Gas = 900_000_000;
+
+    let mut gas = GasMeter::with_limit(INITIAL_GAS_LIMIT);
+
+    let call_gas_limits: Vec<u64> =
+        (100_000_000..800_000_000).step_by(100_000_000).collect();
+    let mut upper_bounds = call_gas_limits.clone();
+
+    let number_of_nested_calls: usize = call_gas_limits.len();
+
+    network
+        .transact(
+            contract_id,
+            0,
+            gas_context::SetGasLimits::new(call_gas_limits),
+            &mut gas,
+        )
+        .unwrap();
+
+    network
+        .transact(
+            contract_id,
+            0,
+            gas_context::TCompute::new(number_of_nested_calls as u64),
+            &mut gas,
+        )
+        .unwrap();
+
+    let limits = network
+        .query(contract_id, 0, gas_context::ReadGasLimits, &mut gas)
+        .unwrap();
+
+    upper_bounds.remove(0);
+    upper_bounds.reverse();
+    upper_bounds.insert(0, INITIAL_GAS_LIMIT);
+    for (index, limit) in limits.iter().enumerate() {
+        assert!(
+            limit < &upper_bounds[index],
+            "Limit {} equal to {} should be below {}",
+            index,
+            limit,
+            upper_bounds[index]
+        );
+    }
+}
