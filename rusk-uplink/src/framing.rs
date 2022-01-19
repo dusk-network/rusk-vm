e1a70d73b4fee658fd3b5762f2619107c7eb60d3
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-01-19 14:03:33 +0100

    Add 'store' variant of macros

--- rusk-uplink/src/framing.rs
+++ rusk-uplink/src/framing.rs
@@ -1,8 +1,8 @@
-
-
+use microkelvin::{OffsetLen, StoreSerializer};
 use rkyv::{Archive, Deserialize, Fallible, Serialize};
 use rkyv::ser::serializers::BufferSerializer;
 use rkyv::ser::Serializer;
+use crate::StoreContext;
 
 
 pub struct EmptyStore;
@@ -66,10 +66,37 @@ pub fn t_return<'a, S, R>(state: &S, ret: &R, scratch: &'a mut [u8]) -> [u32; 2]
     [state_len as u32, return_len as u32]
 }
 
+pub fn t_return_store<S, R>(state: &S, ret: &R, store: StoreContext) -> [u32; 2]
+    where S: Serialize<StoreSerializer<OffsetLen>>,
+          R: Archive + Serialize<StoreSerializer<OffsetLen>>,
+{
+    let mut ser = store.serializer();
+    let state_len = ser.serialize_value(state).unwrap()
+        + core::mem::size_of::<<S as Archive>::Archived>();
+
+    let return_len = ser.serialize_value(ret).unwrap()
+        + core::mem::size_of::<
+        <R as Archive>::Archived,
+    >();
+
+    [state_len as u32, return_len as u32]
+}
+
 pub fn q_return<'a, R>(ret: &R, scratch: &'a mut [u8]) -> u32
     where R: Archive + Serialize<BufferSerializer<&'a mut [u8]>>
 {
     let mut ser = BufferSerializer::new(scratch);
+    let buffer_len = ser.serialize_value(ret).unwrap()
+        + core::mem::size_of::<
+        <R as Archive>::Archived,
+    >();
+    buffer_len as u32
+}
+
+pub fn q_return_store<R>(ret: &R, store: StoreContext) -> u32
+    where R: Archive + Serialize<StoreSerializer<OffsetLen>>
+{
+    let mut ser = store.serializer();
     let buffer_len = ser.serialize_value(ret).unwrap()
         + core::mem::size_of::<
         <R as Archive>::Archived,
@@ -103,6 +130,23 @@ macro_rules! query_state_arg_fun {
 }
 
 #[macro_export]
+macro_rules! query_state_arg_fun_store {
+    ($fun_name:ident, $state_type:ty, $arg_type:ty) => (
+        #[no_mangle]
+        fn $fun_name(written_state: u32, written_data: u32) -> u32 {
+            let (state, arg): ($state_type, $arg_type) = unsafe { get_state_and_arg(written_state, written_data, &SCRATCH) };
+
+            let store =
+                StoreContext::new(AbiStore::new(unsafe { &mut SCRATCH }));
+            let res: <$arg_type as Query>::Return =
+                state.execute(&arg, store);
+
+            unsafe { q_return_store(&res, store) }
+        }
+    );
+}
+
+#[macro_export]
 macro_rules! query_delegate_state_arg_fun {
     ($fun_name:ident, $state_type:ty, $arg_type:ty) => (
         #[no_mangle]
@@ -131,6 +175,23 @@ macro_rules! transaction_state_arg_fun {
                 state.apply(&arg, store);
 
             unsafe { t_return(&state, &res, &mut SCRATCH) }
+        }
+    );
+}
+
+#[macro_export]
+macro_rules! transaction_state_arg_fun_store {
+    ($fun_name:ident, $state_type:ty, $arg_type:ty) => (
+        #[no_mangle]
+        fn $fun_name(written_state: u32, written_data: u32) -> [u32; 2] {
+            let (mut state, arg): ($state_type, $arg_type) = unsafe { get_state_and_arg(written_state, written_data, &SCRATCH) };
+
+            let store =
+                StoreContext::new(AbiStore::new(unsafe { &mut SCRATCH }));
+            let res: <$arg_type as Transaction>::Return =
+                state.apply(&arg, store.clone());
+
+            unsafe { t_return_store(&state, &res, store) }
         }
     );
 }
