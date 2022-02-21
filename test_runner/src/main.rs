dd9e5b653cfca42b989473e83c417f63ef9505dc
Milosz Muszynski <milosz.muszynski@gmail.com>
2022-02-21 16:01:38 +0100

    Add session test for dusk-hamt map

--- test_runner/src/main.rs
+++ test_runner/src/main.rs
@@ -11,6 +11,7 @@ use std::fs;
 use std::path::PathBuf;
 
 use counter::*;
+use map::*;
 use microkelvin::*;
 use rusk_vm::*;
 
@@ -36,7 +37,9 @@ impl Display for PersistE {
     }
 }
 
-fn initialize(
+const MAP_SIZE: u8 = 6;
+
+fn initialize_counter(
     backend: &BackendCtor<DiskBackend>,
 ) -> Result<(), Box<dyn Error>> {
     let counter = Counter::new(99);
@@ -75,26 +78,77 @@ fn initialize(
 
     let persist_id = network.persist(backend).expect("Error in persistence");
 
-    let file_path = PathBuf::from(unsafe { &PATH }).join("persist_id");
+    let file_path = PathBuf::from(unsafe { &PATH }).join("counter_persist_id");
 
     persist_id.write(file_path)?;
 
-    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("contract_id");
+    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("counter_contract_id");
 
     fs::write(&contract_id_path, contract_id.as_bytes())?;
 
     Ok(())
 }
 
-fn confirm(_backend: &BackendCtor<DiskBackend>) -> Result<(), Box<dyn Error>> {
-    let file_path = PathBuf::from(unsafe { &PATH }).join("persist_id");
+fn initialize_map(
+    backend: &BackendCtor<DiskBackend>,
+) -> Result<(), Box<dyn Error>> {
+    let counter = Map::new();
+
+    let code = include_bytes!(
+        "../../target/wasm32-unknown-unknown/release/map.wasm"
+    );
+
+    let contract = Contract::new(counter, code.to_vec());
+
+    let mut network = NetworkState::new();
+
+    let contract_id = network.deploy(contract).unwrap();
+
+    let mut gas = GasMeter::with_limit(1_000_000_000);
+
+    for i in 0..MAP_SIZE {
+        network
+            .transact::<_, Option<u32>>(
+                contract_id,
+                0,
+                (map::SET, i, i as u32),
+                &mut gas,
+            )
+            .unwrap();
+    }
+
+    network.commit();
+
+    let persist_id = network.persist(backend).expect("Error in persistence");
+
+    let file_path = PathBuf::from(unsafe { &PATH }).join("map_persist_id");
+
+    persist_id.write(file_path)?;
+
+    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("map_contract_id");
+
+    fs::write(&contract_id_path, contract_id.as_bytes())?;
+
+    Ok(())
+}
+
+fn initialize(
+    backend: &BackendCtor<DiskBackend>,
+) -> Result<(), Box<dyn Error>> {
+    initialize_counter(backend)?;
+    initialize_map(backend)?;
+    Ok(())
+}
+
+fn confirm_counter(_backend: &BackendCtor<DiskBackend>) -> Result<(), Box<dyn Error>> {
+    let file_path = PathBuf::from(unsafe { &PATH }).join("counter_persist_id");
     let state_id = NetworkStateId::read(file_path)?;
 
     let mut network = NetworkState::new()
         .restore(state_id)
         .map_err(|_| PersistE)?;
 
-    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("contract_id");
+    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("counter_contract_id");
     let buf = fs::read(&contract_id_path)?;
 
     let contract_id = ContractId::from(buf);
@@ -108,6 +162,44 @@ fn confirm(_backend: &BackendCtor<DiskBackend>) -> Result<(), Box<dyn Error>> {
         100
     );
 
+    Ok(())
+}
+
+fn confirm_map(_backend: &BackendCtor<DiskBackend>) -> Result<(), Box<dyn Error>> {
+    let file_path = PathBuf::from(unsafe { &PATH }).join("map_persist_id");
+    let state_id = NetworkStateId::read(file_path)?;
+
+    let mut network = NetworkState::new()
+        .restore(state_id)
+        .map_err(|_| PersistE)?;
+
+    let contract_id_path = PathBuf::from(unsafe { &PATH }).join("map_contract_id");
+    let buf = fs::read(&contract_id_path)?;
+
+    let contract_id = ContractId::from(buf);
+
+    let mut gas = GasMeter::with_limit(1_000_000_000);
+
+    for i in 0..MAP_SIZE {
+        assert_eq!(
+            network
+                .query::<_, Option<u32>>(
+                    contract_id,
+                    0,
+                    (map::GET, i),
+                    &mut gas
+                )
+                .unwrap(),
+            Some(i as u32)
+        );
+    }
+
+    Ok(())
+}
+
+fn confirm(_backend: &BackendCtor<DiskBackend>) -> Result<(), Box<dyn Error>> {
+    confirm_counter(_backend)?;
+    confirm_map(_backend)?;
     Ok(())
 }
 
